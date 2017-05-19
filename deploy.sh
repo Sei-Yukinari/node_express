bb9dbcc3c5d23f983819522648309339da01098f
SeiYukinari <sei.yukinari@nextremer.com>
2017-05-19 10:48:12 +0900

    dockerhubのビルドのhookを修正

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/seiyukinari/node_express/trigger/3aa50748-7409-4618-a485-f2dabd2f220c/curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/seiyukinari/node_express/trigger/3aa50748-7409-4618-a485-f2dabd2f220c/
+curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/seiyukinari/node_express/trigger/3aa50748-7409-4618-a485-f2dabd2f220c/curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/seiyukinari/node_express/trigger/$DOCKER_HUB_TOKEN/
